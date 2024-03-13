225c58b720e05f33d7a4f46d3ff6eceddac7b3aa
Mohit <mohit@hey.com>
2024-03-13 14:50:34 +0530

    x

--- src/content/config.js
+++ src/content/config.js
@@ -1,27 +1,37 @@
 import { z, defineCollection } from "astro:content";
 
 const authorsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    image: z.string(),
-    name: z.string(),
-  }),
-});
+
+	type: "content",
+  	schema: z.object({
+
+		image: z.string(),
+		name: z.string(),
+
+  	})
+
+})
 
 const tasksCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    author: z.string(),
-    create_time: z.string(),
-    description: z.string(),
-    list: z.string(),
-    tag: z.string(),
-    title: z.string(),
-    update_time: z.string(),
-  }),
-});
+
+	type: "content",
+  	schema: z.object({
+
+		author: z.string(),
+		create_time: z.string(),
+		description: z.string().nullable(),
+		list: z.string(),
+		tag: z.string(),
+		title: z.string(),
+		update_time: z.string(),
+
+  	})
+
+})
 
 export const collections = {
-  authors: authorsCollection,
-  tasks: tasksCollection,
-};
+
+	authors: authorsCollection,
+  	tasks: tasksCollection,
+
+}
